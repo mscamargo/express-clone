88190ab8c40f16e8ba36c783919a78ebfd65db01
mscamargo <mscamargo.dev@gmail.com>
2023-05-30 21:51:32 -0300

    just rebuilding

--- src/application_test.ts
+++
@@ -1,9 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.177.1/testing/asserts.ts";
-import { Application, Layer } from "./application.ts";
-
-Deno.test("should push the handler into the #stack", () => {
-  const stack: Layer[] = [];
-  const app = new Application(stack);
-  app.use(async (request, response) => {});
-  assertEquals(stack.length, 1);
-});
